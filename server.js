750466c79f54bc7c234d8ed3d6e9f2b91a5d3deb
ankitsaini0506 <ankitsaini00000506@gmail.com>
2026-04-14 00:55:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
   'https://siddhi-website.vercel.app',
+  'https://siddhi-website-rust.vercel.app',
   process.env.FRONTEND_URL,
 ].filter(Boolean);
 
